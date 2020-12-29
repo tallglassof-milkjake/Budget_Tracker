90a167c36a042b590a2d5436632c9266c44c8354
Jake Mackinlay <jrmackinlay90@gmail.com>
2020-12-29 11:47:16 +1030

    Added service worker and webmanifest

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const logger = require("morgan");
-const mongoose = require("mongoose");
 const compression = require("compression");
+const mongoose = require("mongoose");
 
-const PORT = 3000;
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGODB_ATLAS, 
+  {   w: "majority",
+      useNewUrlParser: true, 
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 

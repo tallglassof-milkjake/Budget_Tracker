7fccd96f9fc317c22b6ab6e8565721333d8d0110
Jake Mackinlay <jrmackinlay90@gmail.com>
2021-01-05 21:52:14 +1030

    deploying to heroku and updated server.js

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ const mongoose = require("mongoose");
 //       useFindAndModify: false
 //     });
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
